bcbc35bef2d6914eee3a7d4120567833edc194e3
Louise <tanlouisekelly@gmail.com>
2025-03-10 20:46:32 +0800

    Delete ACTLAB3.py

--- ACTLAB3.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number: "))
-if num == 0:
-    print("The number is ZERO")
-    
-elif num % 2 == 0:
-    print(num, "is an EVEN number")
-    
-else:
-    print(num, "is an ODD number")
